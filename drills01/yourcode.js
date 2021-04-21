e0409080c6a6db9694bdbfb883982d7421c26726
SprinnaAkahoshi <sprinnaa@zillowgroup.com>
2021-04-20 22:22:06 -0700

    practice drills

--- drills01/yourcode.js
+++ drills01/yourcode.js
@@ -1,7 +1,34 @@
-function sumArray(numbersArray) {}
+function sumArray(numbersArray) {
+  let total = 0;
+  for (let i = 0; i < numbersArray.length; i++) {
+    total += numbersArray[i];
+  }
+  return total;
+}
 
-function fitWithinVal(specialValArray, value) {}
+// 2nd solution
+// const reducer = (acc, curr) => acc + curr;
+//   return numbersArray.reduce(reducer);
 
-function getAllNamesShorterThan(namesArray, nameLength) {}
+function fitWithinVal(specialValArray, value) {
+  const results = [];
+  for (let i = 0; i < specialValArray.length; i++) {
+    if (specialValArray[i] < value) {
+      results.push(specialValArray[i]);
+      value = value - specialValArray[i];
+    }
+  }
+  return results;
+}
+
+function getAllNamesShorterThan(namesArray, nameLength) {
+  const results = [];
+  for (let i = 0; i < namesArray.length; i++) {
+    if (namesArray[i].length < nameLength) {
+      results.push(namesArray[i]);
+    }
+  }
+  return results;
+}
 
 function makeLabel(personObject) {}
